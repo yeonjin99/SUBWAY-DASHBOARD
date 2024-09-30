7fe2390d9799b71bb2f94f841b5e58efa0721d32
Jang Yeonjin <98744367+yeonjin99@users.noreply.github.com>
2024-09-30 17:27:53 +0900

    Update emailOperator.py

--- airflow/emailOperator.py
+++ airflow/emailOperator.py
@@ -6,7 +6,7 @@ from datetime import datetime
 default_args = {
     'owner': 'airflow',
     'start_date': datetime(2024, 9, 1),
-    'email': ['yjjang@sjinc.co.kr'],   # DAG에서 기본 이메일 설정
+    'email': ['email@email.co.kr'],   # DAG에서 기본 이메일 설정
     'email_on_failure': True,                 # 작업 실패 시 이메일 전송
     'email_on_retry': False,                  # 재시도 시 이메일 전송 안 함
 }
@@ -24,7 +24,7 @@ with DAG(
 
     email_task = EmailOperator(
             task_id='send_email',
-            to='yjjang@sjinc.co.kr',
+            to='email@email.co.kr',
             subject='Airflow 성공 메일',
             html_content='Airflow 작업이 완료되었습니다.',
         )
